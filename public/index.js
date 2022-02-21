5c83f9dd144ff8f1447bcc4f0bf6aef6c232a25f
Paul  Vivar <Apvivar92@gmail.com>
2022-02-20 16:23:45 -0800

    updated JS

--- public/index.js
+++ public/index.js
@@ -7,22 +7,40 @@ async function main() {
 }
 
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
+    let bookContainer = document.querySelector('#root')
     bookContainer.innerHTML += `
         <div class="col-sm-3">
             <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
-                    <img class="card-img-top" src="${book.imageURL}" />
-                `
-                : ``}
                 <div class="card-body">
                     <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
-                    <p class="card-text">${book.description}</p>
                 </div>
+                <input id="book${book.id}" type="text" value="${book.quantity}"/>
+                <button onclick="changeBookQuantity(${book.id}, getBookInput(${book.id}).value)">Submit</button>
             </div>
         </div>
     `
 }
 
-main()
+function getBookInput(id){
+    let bookInput = document.getElementById(`book${id}`)
+    return document.getElementById(`book${id}`)
+};
+
+async function changeBookQuantity(id, quantity){
+    let response = await fetch("http://localhost:3001/updateBook", {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "id": 3,
+            "quantity" : quantity
+        }),
+    })
+
+    let bookInput = document.getElementById(`book${id}`)
+    bookInput.value = quantity
+
+};
+
+main();
